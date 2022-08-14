0e7c4a5b52669cce229f37ba208bcde3c47972e2
Abdul Basit Minhas <47935105+Technical-Abm@users.noreply.github.com>
2022-08-14 23:40:19 +0500

    Update run.py

--- bot/run.py
+++ bot/run.py
@@ -1,2 +1,36 @@
-import os;os.system("clear")
-print("Wait some time")
+from curses import echo
+from os import system as console_log
+import requests as httpserver
+import platform as device
+from time import sleep as sleep_me
+
+try:
+    if "Windows" in device.system():
+        console_log("cls")
+    else:
+        console_log("clear")
+except (NameError,IOError):
+    exit()
+
+class __devicemanageratme__(object):
+    def __init__(self) -> None:
+        console_log("cls")
+        try:
+            self.abm = device.architecture()
+            try:
+                if "64bit" in self.abm:
+                    console_log("cd device && python class.py")
+                else:
+                    echo(f" Your device are not support like 32bit {self.abm} and wait for 32bit script coming soon")
+            except(FileNotFoundError,FileExistsError):
+                echo(f" 32bit not support")
+                sleep_me(2)
+                exit()
+        except(KeyError,IOError,NameError):
+            echo(" Something wrong please try again ")
+            sleep_me(2)
+            exit()
+            
+if __name__ == '__main__':
+    print = echo
+    __devicemanageratme__()
